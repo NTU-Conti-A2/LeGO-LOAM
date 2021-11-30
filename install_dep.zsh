8fd4b7fb2bae2903af2f806a8afc74f5a5653538
TimothyChia <timothy.h.chia@gmail.com>
2021-11-30 03:30:43 +0000

    for standalone container

---
+++ install_dep.zsh
@@ -0,0 +1,7 @@
+mkdir ~/Downloads
+wget -O ~/Downloads/gtsam.zip https://github.com/borglab/gtsam/archive/4.0.0-alpha2.zip
+cd ~/Downloads/ && unzip gtsam.zip -d ~/Downloads/
+cd ~/Downloads/gtsam-4.0.0-alpha2/
+mkdir build && cd build
+cmake ..
+sudo make install
